09895c239d897b5bf6fe56c5481a1b614c1bad16
farhandotme <princeofficial355@gmail.com>
2026-04-26 23:53:11 +0530

    created RAG based app for pdf which is terminal based

--- RAG/DocumentLoader/pdf_loader.py
+++ RAG/DocumentLoader/pdf_loader.py
@@ -1,28 +1,36 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_qdrant import Qdrant
+from langchain_groq import ChatGroq
 
+model = ChatGroq(model="llama-3.1-8b-instant")
+
+# user query------
+user_query = input("Ask about the document : ")
 
 # Embedding Model
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-# BAAI/bge-small-en-v1.5
+# embeddings = HuggingFaceEmbeddings(model_name="BAAI/bge-small-en-v1.5")
 
 
-# prompt template
+# prompt template-------
+
 prompt = ChatPromptTemplate(
     [
         (
             "system",
-            "you are an Expert AI Assistent that helps the user to get details about the documents",
+            "you are an Expert AI Assistent and you are a RAG based AI that helps the user to get details about the uploaded documents and do not give any extra information which is not given in the datas just give the infomation which is mentioned in the documents or the given datas.The data will be given to you and you need to explain it easily that the user will understand easily....so the datas are this : {context}",
         ),
-        ("human", ""),
+        ("human", "{query}"),
     ]
 )
+
 
 # the splitter
 splitter = RecursiveCharacterTextSplitter(
@@ -39,3 +47,23 @@ loader = PyPDFLoader(file_path)
 data = loader.load()
 
 chunks = splitter.split_documents(data)
+print("Total Chunks : ", len(chunks))
+
+# created the vactor store
+vactorstore = Qdrant.from_documents(
+    documents=chunks,
+    embedding=embeddings,
+    url="http://localhost:6333",
+    collection_name="michael_data",
+)
+
+similar_datas = vactorstore.similarity_search(user_query, k=2)
+
+context = "\n\n".join([doc.page_content for doc in similar_datas])
+
+
+finalPrompt = prompt.invoke({"context": context, "query": user_query})
+
+response = model.invoke(finalPrompt)
+
+print(response.content)
