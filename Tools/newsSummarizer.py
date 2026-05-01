c3eb42d1906987f47153ee9a2b0255f02f9a8342
farhandotme <princeofficial355@gmail.com>
2026-05-01 22:08:31 +0530

    created news Summary generator using langchain tools and using Tavily api for that.....

---
+++ Tools/newsSummarizer.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_tavily import TavilySearch
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_mistralai import ChatMistralAI
+
+tool = TavilySearch(max_results=3)
+
+prompt = ChatPromptTemplate.from_template("""
+You are a helpful AI Assistant.
+Summarize the following news into clear bullet points:
+
+{news}
+""")
+
+model = ChatMistralAI(model="mistral-small")
+
+chains = prompt | model | StrOutputParser()
+
+
+user_input = input("Enter the News you want to search: ")
+
+tool_calling = tool.invoke(user_input)
+
+
+response = chains.invoke({"news": tool_calling})
+
+print(response)
